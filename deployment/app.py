8ec1433b8f02ae326657e5ca21a4d9bfbd8926a5
Lukas Adiwijaya <lukasadk@gmail.com>
2024-05-10 02:51:10 +0700

    Added Streamlit Deployment

--- deployment/app.py
+++ deployment/app.py
@@ -1,39 +1,19 @@
-import pickle
+import eda 
+import prediction
+import filtering
 import streamlit as st
-import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+PAGES = {
+    "Exploratory Data Analysis": eda,
+    # "Recommender System": prediction,
+    "Filtering Data": filtering
+}
 
-def recommend(city):
-    index = cities[cities['city'] == city].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_accommodations = []
-    for i in distances[1:6]:
-        # fetch the accommodation details
-        accommodation_details = fetch_accommodation_details(i[0])
-        recommended_accommodations.append(accommodation_details)
-
-    return recommended_accommodations
-
-
-st.header('Airbnb Accommodation Recommender System Using Machine Learning')
-cities = pickle.load(open('/listings_clustered.csv','rb'))
-similarity = pickle.load(open('/rec_sys.pkl','rb'))
-
-city_list = cities['city'].values
-selected_city = st.selectbox(
-    "Type or select a city from the dropdown",
-    city_list
-)
-
-if st.button('Show Recommendation'):
-    recommended_accommodations = recommend(selected_city)
-    for accommodation in recommended_accommodations:
-        st.text("Accommodation Description: " + accommodation['description'])
-        st.text("Accommodation City: " + accommodation['city'])
-        st.text("Accommodates: " + str(accommodation['accommodates']))
-        st.text("Number of Reviews: " + str(accommodation['number_of_reviews']))
-        st.text("Bedrooms: " + str(accommodation['bedrooms']))
-        st.text("Beds: " + str(accommodation['beds']))
-        st.text("Price: " + str(accommodation['price']))
-        st.text("Review Scores Rating: " + str(accommodation['review_scores_rating']))
-        st.text("Bathrooms: " + str(accommodation['bathrooms']))
+st.sidebar.title('Pages')
+selection = st.sidebar.radio("Choose page", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
